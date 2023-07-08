d2a6d7080c82c75cfc9a72c244e6a13c012d173e
davit dvalashvili <davitdvalashvili1996@gmail.com>
2023-07-08 22:13:43 +0400

    day 1 exsecises Done

---
+++ Day 1/datatypes.js
@@ -0,0 +1,40 @@
+//Create datatypes.js file and use the JavaScript typeof operator to check different data types. Check the data type of each variable
+let myName = "Davit";
+let GirlFriend = null;
+let hobbies;
+let smart = true;
+
+console.log(typeof myName);
+console.log(typeof GirlFriend);
+console.log(typeof hobbies);
+console.log(typeof smart);
+
+//Declare four variables without assigning values
+let variable1;
+let variable2;
+let variable3;
+let variable4;
+
+
+//Declare four variables with assigned values
+let countryName = "Georgia";
+let location = "Europe";
+let population = "3.5 million";
+let description = "beautiful";
+
+//Declare variables to store your first name, last name, marital status, country and age in multiple lines
+let firstName = "Davit";
+let lastName = "Dvalashvili";
+let Status = "student";
+let country = "Georgia";
+let age = 26;
+
+//Declare variables to store your first name, last name, marital status, country and age in a single line
+let fName = "Davit", lName = "Dvalashvili", Age = 26, sex = "male";
+
+//Declare two variables myAge and yourAge and assign them initial values and log to the browser console.
+let myAge = 26;
+let yourAge = 19;
+
+console.log(`I am ${myAge} years old.`);
+console.log(`I am ${yourAge} years old.`);
